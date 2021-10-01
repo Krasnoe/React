44d6224b996a4a5e56346163ca0db061a2b968bb
Krasnoe <psp.tema@mail.ru>
2021-10-01 17:19:30 +0300

    fix(script.js): fixed const command, calcCash and sumTech

--- script.js
+++ script.js
@@ -1,43 +1,28 @@
 'use strict';
 const employers = ['АртеМ', 'максим', 'Владимир', 'сергей', 'НикиТа', 'евГений', ' Дарья', ' ', 'виктория ', 'ЕкаТерина', '', ' Андрей ', 'КИРИЛЛ'];
 const nameCourse = 'Базовый React';
-const command = [];
-for (let i = 0; i < employers.length; i++) {
-	if (employers[i].length > 0 && employers[i].trim() !== '') {
-		command.push(employers[i]);
-	}
-}
-for (let i = 0; i < command.length; i++) {
-	command[i] = command[i].toLowerCase().trim();
-	command[i] = command[i][0].toUpperCase() + command[i].slice(1);
-}
+const command = employers.filter(name => name.trim().length).map(name => name.trim().charAt(0).toUpperCase() + name.trim().substr(1).toLowerCase());
 
 const data = {
 	cash: [3, 8, 3],
 	react: ['JSX', 'components', 'props', 'state', 'hooks'],
 	add: ['styled-components', 'firebase']
 };
-const { cash, react, add } = data;
+const { cash, react, react: [first], add } = data;
 
 
-const calcCash = arr => {
-	let total = 0;
-	for (let i = 0; i < arr.length; i++) {
-		total += arr[i];
-	}
-	return total;
-};
+const calcCash = everyCash => everyCash.reduce((prev, curr) => prev + curr);
 
 const lesson = calcCash(cash);
 
-const makeBusiness = (director, teacher, allModule, gang, course) => {
-	teacher = teacher || 'Максим';
+const makeBusiness = (director, allModule, gang, course, teacher = 'Максим') => {
+	const sumTech = [...react, ...add, 'и другие'];
 	console.log(
 	`Стартуем новый курс: "${course}". Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}.
 Команда Академии: ${gang}
-Первое что изучим будет ${react[0]}. Он очень похож на HTML!
+Первое что изучим будет ${first}. Он очень похож на HTML!
 Технологии которые мы изучим: 
-${react}, ${add} и другие`);
+${sumTech}`);
 };
 
-makeBusiness('Артем', null, lesson, command, nameCourse);
+makeBusiness('Артем', lesson, command, nameCourse);
