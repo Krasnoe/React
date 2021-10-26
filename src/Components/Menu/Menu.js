47aea344d6245a3a197414885a20aee6c0d7270c
Krasnoe <psp.tema@mail.ru>
2021-10-26 12:35:12 +0300

    feat(useFetch.js): create useFetch.js

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import dbMenu from '../DBMenu';
 import { ListItem } from './ListItem';
 import { Banner } from './Banner';
-
+import { useFetch } from '../Hooks/useFetch';
 
 const MenuStyled = styled.main`
   background-color: #ccc;
@@ -14,26 +13,37 @@ const SectionMenu = styled.section`
   padding: 30px;
 `;
 
-export const Menu = ({ setOpenItem }) => (
-  <MenuStyled>
-    <Banner/>
-    <SectionMenu>
-      <h2>Бургеры</h2>
-      <ListItem 
-          itemList={dbMenu.burger}
-          setOpenItem={setOpenItem}/>
-    </SectionMenu>
+export const Menu = ({ setOpenItem }) => {
 
-    <SectionMenu>
-      <h2>Закуски / Напитки</h2>
-      <ListItem 
-          itemList={dbMenu.other}
-          setOpenItem={setOpenItem}/>
-    </SectionMenu>
+  const res = useFetch();
 
-  </MenuStyled>
+  const dbMenu = res.response;
 
-)
+  return (
+    <MenuStyled>
+      <Banner/>
+      {res.response ? 
+      <>
+        <SectionMenu>
+          <h2>Бургеры</h2>
+          <ListItem 
+              itemList={dbMenu.burger}
+              setOpenItem={setOpenItem}/>
+        </SectionMenu>
+
+        <SectionMenu>
+          <h2>Закуски / Напитки</h2>
+          <ListItem 
+              itemList={dbMenu.other}
+              setOpenItem={setOpenItem}/>
+        </SectionMenu>
+      </> : res.error ? 
+      <div>Извините, мы уже разбираемся с проблемой...</div> : 
+      <div>Загрузка...</div>
+      }
+    </MenuStyled>
+  )
+}
 
 
 
